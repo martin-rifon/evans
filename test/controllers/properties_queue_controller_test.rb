abb6fbdd1997bc7d3e9f7769615e3ef49ae397ce
Martin Rifon <mrifonelizalde@gmail.com>
2016-01-26 23:38:39 -0300

    S#94 removed unused code

--- test/controllers/properties_queue_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PropertiesQueueControllerTest < ActionController::TestCase
-  test "should get listen" do
-    get :listen
-    assert_response :success
-  end
-
-end
--- test/controllers/queuecontroller_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class QueuecontrollerControllerTest < ActionController::TestCase
-  test "should get listen" do
-    get :listen
-    assert_response :success
-  end
-
-end
